0c031bd38a2af741c74c8522ffa2a71909a63610
Timofey <nsk.timka2077@gmail.com>
2023-08-28 20:35:47 +0200

    add dockstrings and use datetime to get current year

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,29 +1,46 @@
+import datetime as dt
+
 from django.db import models
 from django.core.validators import MaxValueValidator
 
 
 class Category(models.Model):
+    """
+    Модель для хранения информации о категории произведения.
+    """
     name = models.CharField("Название", max_length=255)
     slug = models.SlugField("Слаг", max_length=255, unique=True)
 
     def __str__(self) -> str:
+        """
+        Возвращает строковое представление категории (ее название).
+        """
         return self.name
 
 
 class Genre(models.Model):
+    """
+    Модель для хранения информации о жанре произведения.
+    """
     name = models.CharField("Название", max_length=255)
     slug = models.SlugField("Слаг", max_length=255)
 
     def __str__(self) -> str:
+        """
+        Возвращает строковое представление жанра (его название).
+        """
         return self.name
 
 
 class Title(models.Model):
+    """
+    Модель для хранения информации о произведении.
+    """
     name = models.CharField("Название", max_length=255)
     year = models.PositiveSmallIntegerField(
         "Год выхода",
         validators=[
-            MaxValueValidator(2023),
+            MaxValueValidator(dt.datetime.now().year),
         ],
     )
     description = models.TextField("Описание", blank=True, null=True)
@@ -37,10 +54,16 @@ class Title(models.Model):
     genre = models.ManyToManyField(Genre, through="GenreTitle")
 
     def __str__(self) -> str:
+        """
+        Возвращает строковое представление произведения (его название).
+        """
         return self.name
 
 
 class GenreTitle(models.Model):
+    """
+    Промежуточная модель для хранения ключей genre и title
+    """
     genre = models.ForeignKey(
         Genre, on_delete=models.SET_NULL, null=True, verbose_name="Жанр"
     )
@@ -49,4 +72,7 @@ class GenreTitle(models.Model):
     )
 
     def __str__(self) -> str:
+        """
+        Возвращает строковое представление жанра и произведения
+        """
         return f"{self.title} - {self.genre}"
