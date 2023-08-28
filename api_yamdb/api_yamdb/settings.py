845e0d9638f3ea7dbcee7fe8591dc41a6537ffbe
Вячеслав Менюхов <menyukhov@bk.ru>
2023-08-28 23:19:45 +0300

    Add custom User model.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+AUTH_USER_MODEL = 'users.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
