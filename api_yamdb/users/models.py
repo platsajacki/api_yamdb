4b66793e485687b6dc157c043b09aceb0e8b1e67
Вячеслав Менюхов <menyukhov@bk.ru>
2023-08-29 00:00:59 +0300

    Update User model.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,8 +1,27 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from .managers import UserManager
 
+ROLE_CHOICES = [
+    ("user", "Пользователь"),
+    ("moderator", "Модератор"),
+    ("admin", "Администратор"),
+]
+
 
 class User(AbstractUser):
     """Кастомная модель пользователя."""
+    bio = models.TextField(
+        max_length=512,
+        blank=True, null=True
+    )
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
+
+    REQUIRED_FIELDS = ["email", "username"]
+
     objects = UserManager()
