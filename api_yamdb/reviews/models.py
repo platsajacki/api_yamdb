539a998da9ca20f4c1a302883d45dc3d1d1d7443
Timofey <nsk.timka2077@gmail.com>
2023-08-28 22:09:28 +0200

    change imports

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,7 @@
 import datetime as dt
 
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Category(models.Model):
