5e1f6730cec9af8cf0e49c3c84a5c1ba1b673265
Timofey <nsk.timka2077@gmail.com>
2023-08-29 21:06:20 +0200

    make serializers for Title, Genre and Category models

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -18,10 +18,10 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
+        "redoc/",
+        TemplateView.as_view(template_name="redoc.html"),
+        name="redoc",
     ),
 ]
